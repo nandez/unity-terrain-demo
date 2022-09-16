d996522f182ad6569bc0a3e4c1c1d21bbda6ba6b
German Fernandez <gfernandez@evosoftuy.com>
2022-09-16 01:10:56 -0300

    Added minimap feature

---
+++ Assets/Scripts/MinimapController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private float initialShadowDistance;
+
+    private void LateUpdate()
+    {
+        // Usamos la posición del jugador para establecer la nueva posición
+        // de la cámara, el valor actual de "y" representa el zoom del minimap.
+        var newCameraPosition = player.position;
+        newCameraPosition.y = transform.position.y;
+        transform.position = newCameraPosition;
+
+        // Seteamos la rotación de la camara para girar junto con el jugador.
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+
+    private void OnPreRender()
+    {
+        // Guardamos la distancia de sombras y la reseteamos a 0
+        // para evitar que se visulicen las sombras en el minimapa.
+        initialShadowDistance = QualitySettings.shadowDistance;
+        QualitySettings.shadowDistance = 0;
+    }
+
+    private void OnPostRender()
+    {
+        // En el post render, restauramos el valor inicial.
+        QualitySettings.shadowDistance = initialShadowDistance;
+    }
+}
